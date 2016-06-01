0427480f4b2027f166762368e9a1b85f48de6a75
Parham Alvani <parham.alvani@gmail.com>
2016-06-01 21:03:07 +0430

    Game object was added.

--- js/home.js
+++ js/home.js
@@ -1,5 +1,40 @@
 window.onload = onHomeLoad;
 
+
+function Game(name, onlines, imgPath) {
+    this.name = name;
+    this.onlines = onlines;
+    this.imgPath = imgPath;
+}
+
+Game.prototype = {
+    constructor: Game,
+
+    getGameBlock: function () {
+        var top = document.createElement("div");
+        top.className = "game-block";
+        top.id = this.name + "-block";
+        top.setAttribute("data-onlines", this.onlines);
+
+        var inner = document.createElement("div");
+        inner.className = "game-image-container";
+
+        var image = document.createElement("img");
+        image.setAttribute("src", this.imgPath);
+        image.setAttribute("alt", "");
+
+        inner.appendChild(image);
+
+        var par = document.createElement("p");
+        par.innerHTML = "Play " + this.name + "!";
+
+        top.appendChild(inner);
+        top.appendChild(par);
+    }
+
+};
+
+
 function onHomeLoad() {
     var request = new XMLHttpRequest();
     request.open("GET", "http://ie.ce-it.ir/hw3/xml/home.xml", true);
@@ -28,13 +63,19 @@ function onHomeLoad() {
 
             var mouseOverOnlineGame = function () {
                 this.style.backgroundColor =
-                    xml.getElementsByTagName("game")[0].getAttribute("hover");
+                    xml.getElementsByTagName("gameicon")[0].firstElementChild.getAttribute("hover");
             };
 
             var mouseOutOnlineGame = function () {
                 this.style.backgroundColor =
-                    xml.getElementsByTagName("game")[0].getAttribute("color");
-            }
+                    xml.getElementsByTagName("gameicon")[0].firstElementChild.getAttribute("color");
+            };
+
+
+            var maxOnlinesBackground = xml.getElementsByTagName("games")[0].getAttribute("max-onlines-background");
+            var maxOnlinesBorderWitdth = xml.getElementsByTagName("games")[0].getAttribute("max-onlines-border-width")
+
+
         }
     };
 }
